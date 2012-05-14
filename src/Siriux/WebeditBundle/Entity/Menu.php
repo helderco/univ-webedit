7ba4c8dce663c4ff651a889334f2707c890b9894
Helder Correia <me@heldercorreia.com>
2012-05-14 23:02:09 +0000

    Add Menu and MenuItem entities.

---
+++ src/Siriux/WebeditBundle/Entity/Menu.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Siriux\WebeditBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="menus")
+ */
+class Menu
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
+    private $title;
+
+    /**
+     * @ORM\OneToMany(targetEntity="MenuItem", mappedBy="items")
+     */
+    private $items;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="\Siriux\UserBundle\Entity\User")
+     */
+    private $user;
+
+    public function __construct()
+    {
+        $this->items = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Add items
+     *
+     * @param Siriux\WebeditBundle\Entity\MenuItem $items
+     */
+    public function addMenuItem(\Siriux\WebeditBundle\Entity\MenuItem $items)
+    {
+        $this->items[] = $items;
+    }
+
+    /**
+     * Get items
+     *
+     * @return Doctrine\Common\Collections\Collection 
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    /**
+     * Set user
+     *
+     * @param Siriux\UserBundle\Entity\User $user
+     */
+    public function setUser(\Siriux\UserBundle\Entity\User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get user
+     *
+     * @return Siriux\UserBundle\Entity\User 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
---
+++ src/Siriux/WebeditBundle/Entity/MenuItem.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Siriux\WebeditBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="menu_items")
+ */
+class MenuItem
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Menu", inversedBy="items")
+     * @ORM\JoinColumn(name="menu_id", referencedColumnName="id")
+     */
+    private $menu;
+
+    /**
+     * @var string $title
+     *
+     * @ORM\Column(type="string", length=45)
+     */
+    private $title;
+
+
+    /**
+     * @var string $url
+     *
+     * @ORM\Column(type="string", length=45)
+     */
+    private $url;
+
+    /**
+     * @var string $weight
+     *
+     * @ORM\Column(type="integer")
+     */
+    private $weight;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set weight
+     *
+     * @param integer $weight
+     */
+    public function setWeight($weight)
+    {
+        $this->weight = $weight;
+    }
+
+    /**
+     * Get weight
+     *
+     * @return integer 
+     */
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+
+    /**
+     * Set menu
+     *
+     * @param Siriux\WebeditBundle\Entity\Menu $menu
+     */
+    public function setMenu(\Siriux\WebeditBundle\Entity\Menu $menu)
+    {
+        $this->menu = $menu;
+    }
+
+    /**
+     * Get menu
+     *
+     * @return Siriux\WebeditBundle\Entity\Menu 
+     */
+    public function getMenu()
+    {
+        return $this->menu;
+    }
+}
